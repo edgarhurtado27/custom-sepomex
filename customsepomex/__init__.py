c572b75bae91ee897a3ba0fa4319c77420424752
Edgar Hurtado <edgarivancs@gmail.com>
2021-09-07 19:21:47 -0500

    feat: Deleting flask cache and Heroku deployment files updated

--- customsepomex/__init__.py
+++ customsepomex/__init__.py
@@ -1,19 +1,15 @@
 from flask import Flask, jsonify, abort
-from flask_caching import Cache
 from . import mongoclient
 
 
 config = {
     "DEBUG": True,
-    "CACHE_TYPE": "SimpleCache",
-    "CACHE_DEFAULT_TIMEOUT": 300,
     "JSON_AS_ASCII": False
 }
 
 # Instanciamos el servidor
 app = Flask(__name__)
 app.config.from_mapping(config)
-cache = Cache(app)
 
 
 @app.route('/custom/ping')
@@ -21,7 +17,6 @@ def ping():
     return jsonify({ 'mensaje': 'App Online' })
 
 @app.route('/custom/sepomex/<string:zipCode>', methods=['GET'])
-@cache.cached(timeout=50)
 def customSepomexHandler(zipCode):
     if not zipCode.isnumeric() : abort(400)
 
